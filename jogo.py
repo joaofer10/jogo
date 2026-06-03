5ccede3d2069e1c3f6295a26ff81fb30c3d9abe2
joaofer10 <joaovictor2022ferraz@gmail.com>
2026-06-02 22:29:11 -0300

    commit 3(final 0)

--- jogo.py
+++ jogo.py
@@ -33,12 +33,13 @@ print('Jogo teste\n')
 print('Menu\n1-novo jogo \n2-carregar save \n3-sair')
 menu = int(input('digite o numero da sua escolha\nR: '))
 
+
 if menu == 1:
     nome = personagem["nome"]
     hp_max = personagem["hp_max"]
     hp_atual = personagem["hp_atual"]
 
-    print(f'nome: {nome}')
+    # while true:
     print('cutscene:')
     print(f'Você é {nome}, um detetive especializado em investigar assassinatos')
     print('Você chegou em casa cansado depois de anali diferentes relatorios de mortes estranhas ')
@@ -46,7 +47,7 @@ if menu == 1:
     print('----')
     print('chegando no local, ao chegar no local Rubert, um colega policial te espera, Rubert...')
     print('----')
-    proximo_dialogo = input('digite (p) para continuar\nR: ')
+    proximo_dialogo = input('digite () para continuar\nR: ')
 
     print('ato 0: inicio')
     print('----')
@@ -73,16 +74,56 @@ if menu == 1:
 
     print('Bom os outros estão procurando o suspeito pelas redondezas')
     print('(ele tira do bolso uma chave)')
-    print('eu tentei dra uma vasculhada no lugar,\nmas sendo sincero não consigo ficar muito tempo nessa casa')
-    proximo_dialogo = input('digite (p) para continuar\nR: ')
+    print('eu tentei dar uma vasculhada no lugar,\nmas sendo sincero não consigo ficar muito tempo nessa casa')
     print('(ele joga a chave na sua direção e você pega)')
+    proximo_dialogo = input('digite (p) para continuar\nR: ')
     print('não sei para você mas eu me sinto estranho quando participo de investigações assim')
     print('talvez eu tenha um estomago fraco')
-
     print('(ele começa a se afastar e ir )')
+    proximo_dialogo = input('digite (p) para continuar\nR: ')
+
+    while True:
+        print('----')
+        print('Você olha para a casa, ela é uma casa comum de dois andares,\nvocê consegue ver algumas luzes acesas pela janela')
+        print('a cada segundo que você passa olhando essa casa mais estranha ela parece')
+        print('como se um ar de decadencia e morte fosse algo presente nela')
+        print(f'opções de escolha:\n1-estou pensando demais, vamos terminar logo isso...\n2-isso não vai dar bom...')
+        escolha = int(input(f'R: '))
+
+        if escolha == 1:
+            print('você pega a chave da casa e entra')
+            print('você passa pelos corredores até que você chega na sala de estar')
+            print('logo é possivel perceber uma taça de vinho e suas taças em uma mesinha')
+            print('mas o que mais chama atenção é a demarcação de um cadaver no chão e sangue')
+            print('"lá vamos nos..."')
+            investigação = 0
+
+            while True:
+                if investigação < 5:
+                    print('ok onde devo olhar agora?')
+                    print('')
+
+                elif investigação == 5:
+                    while hp_atual > 0:
+                        print('combate')
+
+
+                    
+
+
+
+
+
+
+            
+
+        elif escolha == 2:
+            print('final 0')
+
+        break
 
 
 elif menu == 2:
-    print('ainda em desnevolvimento(o desenvolvedor narigudo não veio)')
+    print('ainda em desenvolvimento(o desenvolvedor narigudo não veio)')
 elif menu == 3:
     print('porque tu entrou no meu jogo se tu ia sair?\nainda sim, muito obrigado por ter perdido o seu tempo :)')
